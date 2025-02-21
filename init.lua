020b92df7beab3c0a79ea4c8750ffee4b73d8d35
C0LDPAW.EX3 <camiiotf2420@gmail.com>
2025-02-21 17:47:20 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -191,7 +191,7 @@ if readFile and writeFile then
     local hasFolderFunctions = (isFolder and makeFolder) ~= nil
     local ran, result = pcall(readFile, "__oh_version.txt")
 
-    if not ran then
+    if not ran or releaseInfo.tag_name ~= result then
         if hasFolderFunctions then
             local function createFolder(path)
                 if not isFolder(path) then
@@ -250,7 +250,8 @@ if readFile and writeFile then
             return unpack(assets)
         end
 
-    elseif ran then
+        writeFile("__oh_version.txt", releaseInfo.tag_name)
+    elseif ran and releaseInfo.tag_name == result then
         function environment.import(asset)
             if importCache[asset] then
                 return unpack(importCache[asset])
@@ -288,5 +289,3 @@ useMethods(import("methods/string"))
 useMethods(import("methods/table"))
 useMethods(import("methods/userdata"))
 useMethods(import("methods/environment"))
-
---import("ui/main")
