251b47439cd99e57addbd2b2564418ee8a66bb50
C0LDPAW.EX3 <camiiotf2420@gmail.com>
2025-02-21 16:40:13 -0300

    Update ConstantScanner.lua

--- ui/modules/ConstantScanner.lua
+++ ui/modules/ConstantScanner.lua
@@ -16,7 +16,7 @@ local ContextMenu, ContextMenuButton = import("ui/controls/ContextMenu")
 local TabSelector = import("ui/controls/TabSelector")
 
 local Page = import("rbxassetid://11389137937").Base.Body.Pages.ConstantScanner
-local Assets = import("rbxassetid://5042114982").ConstantScanner
+local Assets = import("rbxassetid://78370384618967").ConstantScanner
 
 local Query = Page.Query
 local Search = Query.Search
