ed0ab7dc0ffc3920a5f9c7b301dc36d2e22dbdb9
C0LDPAW.EX3 <camiiotf2420@gmail.com>
2025-02-21 16:42:34 -0300

    Update ClosureSpy.lua

--- ui/modules/ClosureSpy.lua
+++ ui/modules/ClosureSpy.lua
@@ -18,8 +18,6 @@ local ContextMenu, ContextMenuButton = import("ui/controls/ContextMenu")
 local Base = import("rbxassetid://11389137937").Base
 local Assets = import("rbxassetid://78370384618967").ClosureSpy
 
-print(Assets.ClosureSpy)
-
 local Prompts = Base.Prompts
 local Page = Base.Body.Pages.ClosureSpy
 
