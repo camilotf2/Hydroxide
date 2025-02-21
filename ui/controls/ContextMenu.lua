2ae00eb8a15c84cb9bf45da80013e2cf2156ec25
C0LDPAW.EX3 <camiiotf2420@gmail.com>
2025-02-21 16:35:16 -0300

    Update ContextMenu.lua

--- ui/controls/ContextMenu.lua
+++ ui/controls/ContextMenu.lua
@@ -1,4 +1,4 @@
-local Assets = import("rbxassetid://5042114982").Controls
+local Assets = import("rbxassetid://78370384618967").Controls
 local Storage = import("rbxassetid://11389137937").ContextMenus
 
 local Players = game:GetService("Players")
