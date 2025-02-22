585a92185b0159c5de6d6fc1c600bfe1e4eb48b7
C0LDPAW.EX3 <camiiotf2420@gmail.com>
2025-02-22 12:19:04 -0300

    Update ClosureSpy.lua

--- ui/modules/ClosureSpy.lua
+++ ui/modules/ClosureSpy.lua
@@ -18,8 +18,6 @@ local ContextMenu, ContextMenuButton = import("ui/controls/ContextMenu")
 local Base = import("rbxassetid://11389137937").Base
 local Assets = import("rbxassetid://78370384618967").ClosureSpy
 
-print(Assets)
-
 local Prompts = Base.Prompts
 local Page = Base.Body.Pages.ClosureSpy
 
@@ -84,6 +82,10 @@ local selected = {
     conditions = {}
 }
 
+local blacklist = {
+    
+}
+
 local conditionContext = ContextMenuButton.new("rbxassetid://4891633802", "Call Conditions")
 local clearContext = ContextMenuButton.new("rbxassetid://4892169181", "Clear Calls")
 local ignoreContext = ContextMenuButton.new("rbxassetid://4842578510", "Ignore Calls")
@@ -133,7 +135,7 @@ local function checkCurrentBlocked()
 end
 
 local Condition = {}
---[[function Condition.new(closure, status, index, value, type)
+function Condition.new(closure, status, index, value, type)
     local condition = {}
     local instance = Assets.ConditionPod:Clone() 
     local content = instance.Content
@@ -427,6 +429,8 @@ function Log.adjust(log)
 
     local callWidth = TextService:GetTextSize(logInstance.Calls.Text, 18, "SourceSans", constants.textWidth).X + 10
     local labelWidth = callWidth + 21
+
+
 
     logInstance.Calls.Size = UDim2.new(0, callWidth, 0, 20)
     logIcon.Position = UDim2.new(0, callWidth, 0.5, -7)
@@ -841,16 +845,32 @@ conditionValueType:SetCallback(function(_dropdown, selected)
     icon.Border.Image = iconCondition
 end)
 
+local activeHooks = {} 
+
 Methods.SetEvent(function(hook, call)
+    if checkcaller() then
+        return
+    end 
+
+    if activeHooks[hook] then
+        return
+    end
+
+    activeHooks[hook] = true
+
     local oldContext = getContext()
     setContext(7)
 
     if not removed[hook] then
-        local log = currentLogs[hook] or Log.new(hook)
-        log:IncrementCalls(call)
+        task.spawn(function()
+            local log = currentLogs[hook] or Log.new(hook)
+            log:IncrementCalls(call)
+        end)
     end
-    
+
     setContext(oldContext)
-end)]]
+    activeHooks[hook] = nil
+end)
+
 
 return ClosureSpy
