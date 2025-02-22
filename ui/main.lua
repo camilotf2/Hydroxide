33bd81dd94cc461b0f3e8dd843cd903fad392e04
C0LDPAW.EX3 <camiiotf2420@gmail.com>
2025-02-22 12:41:47 -0300

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -17,6 +17,7 @@ local ScriptScanner
 local ModuleScanner
 local UpvalueScanner
 local ConstantScanner
+local Explorer
 
 xpcall(function()
 	RemoteSpy = import("ui/modules/RemoteSpy")
@@ -25,6 +26,7 @@ xpcall(function()
 	ModuleScanner = import("ui/modules/ModuleScanner")
 	UpvalueScanner = import("ui/modules/UpvalueScanner")
 	ConstantScanner = import("ui/modules/ConstantScanner")
+	Explorer = import("ui/modules/Explorer")
 end, function(err)
 	local message
 	if err:find("valid member") then
